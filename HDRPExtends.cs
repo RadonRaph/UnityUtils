48fc03e7f77d5e18b5a59c173ced2f57bc1229b5
Horion Raphël <38112654+RadonRaph@users.noreply.github.com>
2020-10-21 02:26:46 +0200

    Initial Push

---
+++ HDRPExtends.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public static class HDRPExtends 
+{
+   
+    public static void FadeWeight(this Volume v, float value, float time)
+    {
+        v.gameObject.GetComponent<MonoBehaviour>().StartCoroutine(_FadeWeight(v, value, time));
+    }
+
+    static IEnumerator _FadeWeight(Volume v, float value, float time)
+    {
+        float baseVal = v.weight;
+        float bTime = time;
+
+        while (time > 0)
+        {
+            v.weight = Mathf.Lerp(baseVal, value, 1 - (time / bTime));
+            yield return new WaitForEndOfFrame();
+            time -= Time.deltaTime;
+        }
+    }
+}
